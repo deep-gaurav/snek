c7a9b013254e0d60e30d7165153ad64449b4e465
Deep Gaurav <deepgauravraj@gmail.com>
2023-10-04 22:37:34 +0530

    fix download link

--- build_web_release.sh
+++ build_web_release.sh
@@ -3,7 +3,7 @@ set -e
 curl https://sh.rustup.rs -sSf | sh -s - --default-toolchain stable -y
 source ~/.cargo/env
 
-curl -L https://github.com/thedodd/trunk/releases/download/latest/trunk-x86_64-unknown-linux-gnu.tar.gz --output trunk.tar.gz
+curl -L https://github.com/thedodd/trunk/releases/latest/download/trunk-x86_64-unknown-linux-gnu.tar.gz --output trunk.tar.gz
 tar -zxvf trunk.tar.gz
 
 export PATH="$PATH:$PWD"
